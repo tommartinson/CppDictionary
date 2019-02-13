4caf49b6e6a0963a3c641b4ff232925608968370
Tom Martinson <38024425+tommartinson@users.noreply.github.com>
2019-02-12 16:21:36 -0800

    Update Dictionary.cpp

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -9,34 +9,47 @@ class Dictionary{
   vector< pair <string,int> > dict;   //vector to store pairs
     
   void keys(){//prints list of keys
-    cout << "[";
-    for(int i = 0;i<dict.size()-1;i++){
-      cout << dict[i].first << ", "; 
+    if(dict.size()>0){
+      cout << "Keys: [";
+      for(int i = 0;i<dict.size()-1;i++){
+        cout << dict[i].first << ", "; 
+      }
+      cout << dict[dict.size()-1].first << "]" << endl;
+    }else{
+      cout << "Keys: []"<< endl;
     }
-    cout << dict[dict.size()-1].first << "]" << endl;
 	}    
 
   void values(){//prints list of values
-    cout << "[";
-    for(int i = 0;i<dict.size()-1;i++){
-      cout << dict[i].second << ", "; 
+    if(dict.size()>0){
+      cout << "Values: [";
+      for(int i = 0;i<dict.size()-1;i++){
+        cout << dict[i].second << ", "; 
+      }
+      cout << dict[dict.size()-1].second << "]" << endl;
+    }else{
+      cout << "Values: []" << endl;
     }
-    cout << dict[dict.size()-1].second << "]" << endl;
-    
 	}  
 
   void items(){//prints list of key/value pairs
-    cout << "[";
-    for(int i = 0;i<dict.size()-1;i++){
-      cout << "(" << dict[i].first << "," << dict[i].second << "),"; 
+    if(dict.size()>0){
+      cout << "Items: [";
+      for(int i = 0;i<dict.size()-1;i++){
+        cout << "(" << dict[i].first << "," << dict[i].second << "),"; 
+      }
+      cout << "(" << dict[dict.size()-1].first << "," << dict[dict.size()-1].second << ")]" << endl;
+    }else{
+      cout << "Items: []" << endl;
     }
-    cout << "(" << dict[dict.size()-1].first << "," << dict[dict.size()-1].second << ")]" << endl;
 
   }  
 
   void add(string key, int value){//adds a pair to the dictionary
 	  if(in(key)==0){
       dict.push_back( make_pair(key,value) );
+    }else{
+      cout << "Key: '" << key << "' is already in the dictionary."<<endl;
     }
 	}     
 
@@ -88,38 +101,62 @@ class Dictionary{
        
 };
 
+//Driver
 int main(){
-    
+  cout << "Let's create a dictionary and add some items to it." << endl;
   Dictionary d;
   d.add("tom",22);
   d.add("joe",18);
-  d.add("ciera",23);
-  //d.add("mitch",21);
+  d.add("randy",27);
   d.add("mitch",25);
   d.add("nathan",21);
-  //d.keys();
-  //d.values();
+  
+  cout <<"Let's look at a list of the items we added." << endl;
   d.items();
-  //cout << d.in("mitch") << endl;
-  //cout << d.get("tom")<< endl;
-  //cout << d.get("nathan")<< endl;
-  //cout << d.get("poop")<< endl;
 
+  cout << endl << "Let's look at just the keys." << endl;
+  d.keys();
+
+  cout << endl<<"Now just the values." << endl;
+  d.values();
+  
+  cout <<endl << "What happens if we try to add an item with a key that is already in the dictionary?" << endl;
+  d.add("mitch",25);
+
+  cout <<endl << "Let's test if there is a key in the dictionary. 1 means yes, 0 means no." << endl;
+  cout << d.in("mitch") << endl;
+
+  cout << endl << "Let's see what value matches a certain key. -1 means the key doesn't exist." << endl;
+  cout << "The value is: " << d.get("tom")<< endl;
+  cout << "The value is: " << d.get("nathan")<< endl;
+  cout << "The value is: " << d.get("larry")<< endl;
+
+  cout << endl << "Now let's create another Dictionary and add items to it." << endl;
   Dictionary d2;
-  d2.add("tom",22);
+  d2.add("billy",76);
   d2.add("mom",57);
-  d2.add("ciera",23);
+  d2.add("fred",99);
+  d2.add("randy",27);
   d2.add("dad",58);
   d2.add("nathan",21);
+  
   d2.items();
 
-  Dictionary d3 = d+(d2);
-  d3.items();
   
-  cout << "Let's try deleting some items." << endl;
-  d3.del("tom");
-  d3.del("dad");
-  d3.items();
+  
+  cout <<endl<< "Let's try deleting an item." << endl;
+  d2.del("fred");
+  d2.items();
+
+  //Dictionary d3 = d+(d2);
+  //d3.items();
+  //d3.del("dad");
+  //d3.items();
+
+  Dictionary d4;
+  d4.items();
+  d4.keys();
+  d4.values();
     
   return 0;
 }
